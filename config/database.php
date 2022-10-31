e6d16955c6e7812445d9bc7fc2b29a61af216554
burakersen <burak.ersen@sekizbit.com.tr>
2022-10-31 21:54:35 +0300

    For heroku deployment, postgres settings.

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DB_CONFIG = parse_url(env('DATABASE_URL'));
 return [
 
     /*
@@ -32,7 +32,6 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-
     'connections' => [
 
         'sqlite' => [
@@ -66,11 +65,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DB_CONFIG['host'],
+            'port' => $DB_CONFIG['port'],
+            'database' => ltrim($DB_CONFIG['path'], '/'),
+            'username' => $DB_CONFIG['user'],
+            'password' => $DB_CONFIG['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
