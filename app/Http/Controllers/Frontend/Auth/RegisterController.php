c1ce264db2172843618118d5a2e74df40c0d4724
burakersen <burak.ersen@sekizbit.com.tr>
2022-11-01 16:04:01 +0300

    Redirect to app after registration.

--- app/Http/Controllers/Frontend/Auth/RegisterController.php
+++ app/Http/Controllers/Frontend/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use App\Http\Requests\UserStoreRequest;
 use App\Models\Access\User;
@@ -24,6 +25,14 @@ class RegisterController extends Controller
     {
       $user = User::create(Arr::collapse([$request->validated(), ['api_token' => Str::random(60)]]));
 
-      return $user;
+      if ($user && Auth::attempt($request->only('email', 'password'))) {
+          $request->session()->regenerate();
+
+          return redirect()->route('backend.index');
+      }
+
+      return back()->withErrors([
+        'error_message' => trans('auth.failed_register'),
+      ]);
     }
 }
