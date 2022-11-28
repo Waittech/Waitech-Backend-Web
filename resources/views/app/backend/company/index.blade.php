fb3670f098930978e09862a9c546c5812582e2cc
burakersen <burak.ersen@sekizbit.com.tr>
2022-11-28 17:51:07 +0300

    [WIP]

--- resources/views/app/backend/company/index.blade.php
+++ resources/views/app/backend/company/index.blade.php
@@ -15,9 +15,8 @@
   <div class="row">
     <div class="col-md-12">
       <ul class="nav nav-pills flex-column flex-md-row mb-3">
-        <li class="nav-item"><a class="nav-link active" href="javascript:void(0);"><i class="bx bx-user me-1"></i> Account</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{url('app/pages/account-settings-notifications')}}"><i class="bx bx-bell me-1"></i> Notifications</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{url('app/pages/account-settings-connections')}}"><i class="bx bx-link-alt me-1"></i> Connections</a></li>
+        <li class="nav-item"><a class="nav-link active" href="javascript:void(0);"><i class="bx bx-link-alt me-1"></i> Firma Bilgileri</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{url('app/pages/account-settings-connections')}}"><i class="bx bx-user me-1"></i> Çalışanlar</a></li>
       </ul>
       <div class="card mb-4">
         <form action="{{ route('backend.company.update') }}" method="post" enctype="multipart/form-data">
