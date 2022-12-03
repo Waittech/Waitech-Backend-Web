d832d62ce2149425f2ff4b5ade063eb64fcf59ab
burakersen <burak.ersen@sekizbit.com.tr>
2022-12-03 23:06:14 +0300

    NumberFormatter added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use NumberFormatter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
    */
   public function register()
   {
-    //
+    $this->app->singleton(NumberFormatter::class, function () {
+      return new NumberFormatter('tr_TR', NumberFormatter::DECIMAL);
+    });
   }
 
   /**
