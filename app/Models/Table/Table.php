0a7fee0551d2d805f83a8424957b03bd2af44fe6
burakersen <burak.ersen@sekizbit.com.tr>
2022-12-11 14:53:38 +0300

    Table Model and Migration Created

---
+++ app/Models/Table/Table.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Table;
+
+use App\Models\Company\Traits\BelongsToCompany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Table extends Model
+{
+    use HasFactory, BelongsToCompany;
+}
