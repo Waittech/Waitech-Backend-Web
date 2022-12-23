0394b6ee864e9d02912f48b078f28336efc212af
burakersen <burak.ersen@sekizbit.com.tr>
2022-12-23 16:35:53 +0300

    Fixed lines

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -9,31 +9,31 @@ class HomeController
 {
     public function index()
     {
-      $company_id = session()->get('company_id');
-      $company = Company::find($company_id);
+        $company_id = session()->get('company_id');
+        $company = Company::find($company_id);
 
-      $newOrderCount = $company->orders()->newOrderCount();
-      $preparedOrderCount = $company->orders()->preparedOrderCount();
-      $deliveredOrderCount = $company->orders()->deliveredOrderCount();
+        $newOrderCount = $company->orders()->newOrderCount();
+        $preparedOrderCount = $company->orders()->preparedOrderCount();
+        $deliveredOrderCount = $company->orders()->deliveredOrderCount();
 
-      $dailyOrderCount = $company->orders()->dailyOrderCount();
-      $dailyEarnings = $company->orders()->dailyEarnings();
+        $dailyOrderCount = $company->orders()->dailyOrderCount();
+        $dailyEarnings = $company->orders()->dailyEarnings();
 
-      $newOrders = $company->orders()->where('status', OrderStatus::NEW)->orderBy('created_at', 'DESC')->take(10)->get();
-      $customerCanceledOrders = $company->orders()->where('status', OrderStatus::CANCELED_BY_USER)->orderBy('created_at', 'DESC')->take(10)->get();
-      $companyCanceledOrders = $company->orders()->where('status', OrderStatus::CANCELED_BY_COMPANY)->orderBy('created_at', 'DESC')->take(10)->get();
+        $newOrders = $company->orders()->where('status', OrderStatus::NEW)->orderBy('created_at', 'DESC')->take(10)->get();
+        $customerCanceledOrders = $company->orders()->where('status', OrderStatus::CANCELED_BY_USER)->orderBy('created_at', 'DESC')->take(10)->get();
+        $companyCanceledOrders = $company->orders()->where('status', OrderStatus::CANCELED_BY_COMPANY)->orderBy('created_at', 'DESC')->take(10)->get();
 
-      return view(
-        'app.backend.dashboard',
-        compact(
-          'newOrderCount',
-          'preparedOrderCount',
-          'deliveredOrderCount',
-          'dailyOrderCount',
-          'dailyEarnings',
-          'newOrders',
-          'customerCanceledOrders',
-          'companyCanceledOrders'
-        ));
+        return view(
+          'app.backend.dashboard',
+          compact(
+            'newOrderCount',
+            'preparedOrderCount',
+            'deliveredOrderCount',
+            'dailyOrderCount',
+            'dailyEarnings',
+            'newOrders',
+            'customerCanceledOrders',
+            'companyCanceledOrders'
+          ));
     }
 }
