275a393b3e8096cc27b38ba751124cc99e1be3f9
burakersen <burak.ersen@sekizbit.com.tr>
2023-01-01 16:03:49 +0300

    menu converted

--- app/Http/Resources/CompanyResource.php
+++ app/Http/Resources/CompanyResource.php
@@ -27,7 +27,7 @@ class CompanyResource extends JsonResource
         'district'     => ($district = District::find($this->district_id)) != null ? $district->name : null,
         'neighborhood' => $this->neighborhood_name,
         'street'       => $this->street_name,
-        'menu'         => $this->when($this->menus, MenuResource::collection($this->menu()->active())->groupBy('category.name')),
+        'menu'         => MenuResource::collection($this->menu()->active())->groupBy('category.name'),
       ];
     }
 }
